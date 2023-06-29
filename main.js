e5cbe79683576dcc09db2a4abc28721ad008b454
**** <****>
2023-06-29 15:34:23 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const emptyHeart = document.querySelector('.like-glyph');
+const fullHeart = document.querySelector('.activated-heart');
+const errorModal = document.querySelector('#modal');
+const errorMessage = document.querySelector('#modal-message');
 
+// Add the .hidden class to the error modal in the HTML
+errorModal.classList.add('hidden');
 
+// Event listener for the empty heart
+emptyHeart.addEventListener('click', function() {
+  mimicServerCall()
+    .then(function(response) {
+      emptyHeart.classList.remove('like-glyph');
+      emptyHeart.classList.add('activated-heart');
+      emptyHeart.textContent = FULL_HEART;
+    })
+    .catch(function(error) {
+      errorModal.classList.remove('hidden');
+      errorMessage.textContent = error;
+      setTimeout(function() {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+});
 
+// Event listener for the full heart
+fullHeart.addEventListener('click', function() {
+  fullHeart.classList.remove('activated-heart');
+  fullHeart.classList.add('like-glyph');
+  fullHeart.textContent = EMPTY_HEART;
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
